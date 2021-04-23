828c00694407f38cc54a7f6c9cf8e5970173dc92
Alexander <43503189+avasyutkin@users.noreply.github.com>
2021-04-23 13:46:55 +0300

    Запутанность через состояние W

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
 from numpy import pi
 
+### Состояние GHZ (запутанность)
 qreg_q = QuantumRegister(3, 'q')
 creg_c = ClassicalRegister(3, 'c')
 circuit = QuantumCircuit(qreg_q, creg_c)
@@ -15,3 +16,21 @@ circuit.measure(qreg_q[0], creg_c[0])
 circuit.measure(qreg_q[1], creg_c[1])
 circuit.measure(qreg_q[2], creg_c[2])
 print(circuit)
+
+
+### Состояние W (запутанность)
+qreg_q = QuantumRegister(3, 'q')
+creg_c = ClassicalRegister(3, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.reset(qreg_q[0])
+circuit.reset(qreg_q[1])
+circuit.reset(qreg_q[2])
+circuit.ry(1.9106332362490184, qreg_q[0])
+circuit.cu(pi/2, pi/2, pi/2, pi/2, qreg_q[0], qreg_q[1])
+circuit.cx(qreg_q[1], qreg_q[2])
+circuit.cx(qreg_q[0], qreg_q[1])
+circuit.x(qreg_q[0])
+circuit.measure(qreg_q[0], creg_c[0])
+circuit.measure(qreg_q[1], creg_c[1])
+circuit.measure(qreg_q[2], creg_c[2])

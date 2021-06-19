26c7bdb0b7efca0a9903657cd286032e9cc315d2
Alexander <43503189+avasyutkin@users.noreply.github.com>
2021-06-19 13:27:08 +0300

    факторизация числа

--- Shor's Algorithm.py
+++ Shor's Algorithm.py
@@ -2,6 +2,8 @@ import numpy as np
 from qiskit import QuantumCircuit, Aer, transpile, assemble
 import pandas as pd
 from fractions import Fraction
+import random
+from math import gcd
 
 
 # U|y> = |ay mod 15>
@@ -40,6 +42,28 @@ def qft_dagger(n):
             qc.cp(-np.pi/float(2**(j-m)), m, j)
         qc.h(j)
     return qc
+
+
+# поиск фазы
+def qpe_amod15(a):
+    n_count = 8
+    qc = QuantumCircuit(4+n_count, n_count)
+    for q in range(n_count):
+        qc.h(q)  # инициализация расчетных кубитов в состоянии |+>
+    qc.x(3+n_count)  #вспомогательные регистры в состоянии |1>
+    for q in range(n_count):  # выполнение controlled-U операции
+        qc.append(c_amod15(a, 2**q), [q] + [i+n_count for i in range(4)])
+    qc.append(qft_dagger(n_count), range(n_count))
+    qc.measure(range(n_count), range(n_count))
+
+    qasm_sim = Aer.get_backend('qasm_simulator')
+    t_qc = transpile(qc, qasm_sim)
+    obj = assemble(t_qc, shots=1)
+    result = qasm_sim.run(qobj, memory=True).result()
+    readings = result.get_memory()
+    phase = int(readings[0], 2)/(2**n_count)
+
+    return phase
 
 
 n_count = 8
@@ -99,3 +123,20 @@ for phase in measured_phases:
 headers = ["Фаза", "Дробь", "Результат угадывания периода"]
 df = pd.DataFrame(rows, columns=headers)
 print(df)
+print()
+
+
+# поиск простых делителей числа
+N = 15
+a = 7
+factor_found = False
+while not factor_found:
+    phase = qpe_amod15(a)
+    frac = Fraction(phase).limit_denominator(N)
+    r = frac.denominator
+    if phase != 0:
+        guesses = [gcd(a**(r//2)-1, N), gcd(a**(r//2)+1, N)]
+        for guess in guesses:
+            if guess not in [1, N] and (N % guess) == 0:  # проверка того, что угаданное число действительно является делителем
+                print('Простые делители числа', N, ':', guess)
+                factor_found = True
